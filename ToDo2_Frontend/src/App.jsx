805e4ea0a6a0bc3a141155de5a4440012fb341d1
ozlu554 <tugceozlu188@gmail.com>
2025-12-15 16:15:42 +0300

    frontende tasarımlar eklendi

--- ToDo2_Frontend/src/App.jsx
+++ ToDo2_Frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
 import TodoLayout from "./pages/TodoLayout";
 
 function App() {
@@ -8,7 +7,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
         <Route path="/todo" element={<TodoLayout />} />
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
--- ToDo2_Frontend/src/pages/Login.jsx
+++ ToDo2_Frontend/src/pages/Login.jsx
@@ -1,33 +1,56 @@
 import "./Login.css";
 import owl from "../assets/owl.png";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
   const navigate = useNavigate();
-
-  const handleLogin = () => {
-    navigate("/todo");
-  };
+  const [isRegister, setIsRegister] = useState(false);
 
   return (
     <div className="login-container">
-      <div className="login-card">
+      <div className={`login-card ${isRegister ? "fade" : ""}`}>
         <img src={owl} alt="Owl" className="login-owl" />
 
-        <h2>Giriş Yap</h2>
+        <h2>{isRegister ? "Kayıt Ol" : "Giriş Yap"}</h2>
+
+        {isRegister && (
+          <input type="text" placeholder="İsim Soyisim" />
+        )}
 
         <input type="email" placeholder="E-posta" />
         <input type="password" placeholder="Şifre" />
 
-        <button onClick={handleLogin}>Giriş</button>
+        <button
+          onClick={() => {
+            if (isRegister) {
+              alert("Kayıt işlemi backend ile yapılacak");
+              setIsRegister(false);
+            } else {
+              navigate("/todo");
+            }
+          }}
+        >
+          {isRegister ? "Kayıt Ol" : "Giriş"}
+        </button>
 
-      <div className="register-text">
-  Hesabın yok mu?{" "}
-  <span onClick={() => navigate("/register")}>
-    Kayıt ol
-  </span>
-</div>
-
+        <div className="register-text">
+          {isRegister ? (
+            <>
+              Zaten hesabın var mı?{" "}
+              <span onClick={() => setIsRegister(false)}>
+                Giriş yap
+              </span>
+            </>
+          ) : (
+            <>
+              Hesabın yok mu?{" "}
+              <span onClick={() => setIsRegister(true)}>
+                Kayıt ol
+              </span>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
