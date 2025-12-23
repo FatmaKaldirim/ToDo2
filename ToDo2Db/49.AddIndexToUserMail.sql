a33fba4ed4c423baa3a557d95fe0ea70098e5c89
Gamze İlayda Topaktaş <gmzilyd@gmail.com>
2025-12-23 19:33:17 +0300

    54 Update49AddIndexToUserMail
    49.query duzenlendi

--- ToDo2Db/49.AddIndexToUserMail.sql
+++ ToDo2Db/49.AddIndexToUserMail.sql
@@ -1,8 +1,10 @@
-USE ToDo2Db
-GO
-
--- Add a non-clustered index to the UserMail column in the Users table
--- to speed up login queries.
-CREATE NONCLUSTERED INDEX IX_Users_UserMail ON Users(UserMail)
-INCLUDE (UserID, UserName, PasswordHash, PasswordSalt, IsActive);
-GO
+IF NOT EXISTS (
+    SELECT 1
+    FROM sys.indexes
+    WHERE name = 'IX_Users_UserMail'
+      AND object_id = OBJECT_ID('dbo.Users')
+)
+BEGIN
+    CREATE INDEX IX_Users_UserMail
+    ON dbo.Users(UserMail);
+END
